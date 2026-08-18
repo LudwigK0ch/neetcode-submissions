0e22790a63a68d1a9b5a7c8c3c7ff64928657d34
Ludwig Koch <lucky.koch@gmx.de>
2026-08-18 19:46:32 +0200

    Add: max-consecutive-ones - submission-4

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int current = 0;
+
+        for(int num : nums) {
+            if(num == 1) {
+                current++;
+            } else {
+                max = Math.max(max, current);
+                current = 0;
+            }
+        }
+
+        return Math.max(max, current);
+    }
+}
